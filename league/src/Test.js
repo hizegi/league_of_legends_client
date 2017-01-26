88ebf3b4dafa0aead09142919933f1aac66fe71c
Koty Wong <wongkoty@gmail.com>
2017-01-26 15:21:59 -0500

    adding fake nav bar

--- league/src/Test.js
+++ league/src/Test.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 
-class App extends Component {
+class Test extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="Test">
         <h1>Welcome to summoners rift</h1>
       </div>
     );
   }
 }
 
-export default App;
+export default Test;
--- league/src/index.js
+++ league/src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Test from './Test';
+import Nav from './Nav';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <div className="container">
+    <Nav />
+    <App />
+    <Test />
+  </div>,
   document.getElementById('root')
 );
